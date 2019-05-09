a7e724f664e089e172aa1bdd85c39767404ef01c
Shaiell <jcl.ribeiro@hotmail.com>
2019-05-09 14:24:10 -0300

    Initial commit

---
+++ src/aula1/Principal.java
@@ -0,0 +1,23 @@
+package aula1;
+
+
+/**
+ * Classe inicial de exemplo de desenvolvimento java.
+ * 
+ * @author Jean Ribeiro
+ * @version 1.0
+ * @since 27/02/2019
+ *
+ */
+public class Principal {
+	/**
+	 * Método que inicia o programa.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello word!");
+		
+	}
+	
+}
